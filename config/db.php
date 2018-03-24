2b09c4bcae9b36ba29c14621f3e7ae699a0f84b8
haohonglong <947923503@qq.com>
2018-03-24 13:58:15 +0800

    modify

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=192.168.66.111;dbname=118oa',
-    'username' => 'pp',
-    'password' => 'paperpass',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=118oa',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 ];
